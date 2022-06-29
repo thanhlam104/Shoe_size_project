0245796be34a0c458a49f5ae168a8398b3b45b4a
thanhlam104 <91646846+thanhlamdang@users.noreply.github.com>
2022-06-29 20:41:27 +0700

    test

--- shoe_size.py
+++ shoe_size.py
@@ -1,3 +1,8 @@
+from dis import Instruction
 import streamlit as st 
 
-st.write("Find your shoe size")
+
+if __name__=='__main__':
+    st.title("Find your shoe size")
+    instruction = 'Get your shoe size quickly and easily by uploading an image of your foot on a white A4 papersheet.'
+    st.write(instruction)
