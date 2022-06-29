4af2dca6aff161fcc96408ed5b635415a74a9c00
thanhlam104 <91646846+thanhlamdang@users.noreply.github.com>
2022-06-29 20:54:05 +0700

    update

--- shoe_size.py
+++ shoe_size.py
@@ -1,8 +1,16 @@
 from dis import Instruction
 import streamlit as st 
-
+from PIL import Image
 
 if __name__=='__main__':
-    st.title("Find your shoe size")
+    st.title("Find your shoe size!")
     instruction = 'Get your shoe size quickly and easily by uploading an image of your foot on a white A4 papersheet.'
     st.write(instruction)
+
+    file = st.file_uploader("Upload your image")
+
+    if file:
+        img = Image.open(file)
+        st.title("Here is the image you have uploaded")
+        resized_image = img.thumbnail((400,400), Image.ANTIALIAS)
+        st.image(resized_image)
